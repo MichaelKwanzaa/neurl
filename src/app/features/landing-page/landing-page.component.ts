e420b06ede318840924fffa55647309eb307b0f3
MichaelKwanzaa <meagbonyeme@gmail.com>
2023-05-08 23:07:56 +0100

    feat: added netlify configs, and fixed up how it works header, and spacing

--- src/app/features/landing-page/landing-page.component.ts
+++ src/app/features/landing-page/landing-page.component.ts
@@ -1,11 +1,14 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit, ElementRef} from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+
+
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit{
   baseImagesUrl = "/assets/images/";
   baseWhatWeDoImagesUrl: string = `${this.baseImagesUrl}/what-we-do/`
   baseHowItWorksUrl: string = `${this.baseImagesUrl}/how-it-works/`
@@ -31,13 +34,43 @@ export class LandingPageComponent {
   linkedIn: string = this.socialsUrl + "linkedIn.svg";
 
   twitterLink: string = "";
-  linkedInLink: string = ""
+  linkedInLink: string = "https://www.linkedin.com/company/writewithneurl/";
 
   isOpen: boolean = false;
 
   @Output() public sidenavToggle = new EventEmitter();
 
-  constructor(){
+  constructor(private elementRef: ElementRef, private meta: Meta){
+   // Meta tags for home page
+this.meta.updateTag({ name: 'title', content: 'We Create Technical Content for Your Audience | Neurl' });
+this.meta.updateTag({ name: 'description', content: 'Neurl is on a mission to demystify complex data science and engineering topics for your preferred audience. Schedule a call today!' });
+
+// Meta tags for "What we do" section
+this.meta.updateTag({ name: 'title', content: 'Outsource Technical Content Development to Neurl' });
+this.meta.updateTag({ name: 'description', content: 'By outsourcing technical content development to us, you can free up your engineers and data scientists to concentrate on critical aspects of your business. We create in-depth technical blog articles, edit existing documentation, provide tutorials, and offer custom services. Schedule a call today!' });
+
+// Meta tags for "How it works" section
+this.meta.updateTag({ name: 'title', content: 'How It Works | Neurl' });
+this.meta.updateTag({ name: 'description', content: 'Learn how our process works in three simple steps: Schedule a call, Content Plan Creation, and Ready to Publish. Schedule a call today to get started!' });
+
+// Meta tags for footer
+this.meta.updateTag({ name: 'title', content: 'Neurl - Technical Content Development' });
+this.meta.updateTag({ name: 'description', content: 'Neurl specializes in technical content development. Connect with us on Twitter and LinkedIn. All Rights Reserved Neurl 2023.' });
+  }
+
+  ngOnInit(){
+    const script = this.elementRef.nativeElement.ownerDocument.createElement('script');
+    script.type = 'application/ld+json';
+    script.text = `
+      {
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "name": "Neurl",
+        "url": "https://writewithneurl.com",
+        "logo": "https://writewithneurl.com/logo.svg"
+      }
+    `;
+    this.elementRef.nativeElement.ownerDocument.head.appendChild(script);
   }
 
   handleMenu(){
